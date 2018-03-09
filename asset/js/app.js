9989a37a8c1c9c3113f3edd789cf863dd6a5364c
PRECHARAT <precharat@LOOKOD.local>
2018-03-09 18:09:25 +0700

    Page loader and data api

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,15 +1,45 @@
-var app = angular.module('myApp', ["ngRoute","ng-sweet-alert"]);
+var app = angular.module('myApp', ["ngRoute", "ng-sweet-alert", "angular-page-loader"]);
+
+app.run(function ($timeout, $rootScope) {
+
+        $timeout(function () { // simulate long page loading 
+            $rootScope.isLoading = false; // turn "off" the flag 
+        }, 1000)
+
+    })
+
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "main.html"
         })
+        .when("/index", {
+            templateUrl: "main.html"
+        })
+        .when("/login", {
+            templateUrl: "login.html"
+        })
+        .when("/member", {
+            templateUrl: "member.html",
+            controller: "memberController"
+        })
         
+});
+
+app.controller('memberController', function ($scope, $http) {
+    $http.get('http://localhost:8888/yaso-coop/services/login/get').then(function(response){
+        
+        if(response.data.status == 200){
+            $scope.data = response.data.data;
+        }
+
+    });
+
 });
 
 app.controller('test', function ($scope) {
     $scope.testalert = function () {
-        swal("Oops!", "Something went wrong!", "error")
+        swal("Oops!", "Something went wrong!", "success")
     }
 });
